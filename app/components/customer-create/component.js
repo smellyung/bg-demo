c65bd2e8ad4c927ef32d6fa7c2ddb2beef7bd71c
Steve Tyler <mail@steve-tyler.co.uk>
2018-11-19 18:58:55 +0000

    added components and updated router

---
+++ app/components/customer-create/component.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import {attribute} from '@ember-decorators/component';
+
+export default class CustomerRowComponent extends Component {
+  //@attribute tagname = 'tr'
+}
---
+++ app/components/customer-row/component.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import {attribute} from '@ember-decorators/component';
+
+export default class CustomerRowComponent extends Component {
+  //@attribute tagName='tr';
+}
